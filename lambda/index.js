dc486c6cf4612e848f0ca46ccc071646ecf38faf
theepeekaa <84039199+theepeekaa@users.noreply.github.com>
2022-04-19 23:07:38 -0500

    Add files via upload

--- lambda/index.js
+++ lambda/index.js
@@ -1,156 +1,319 @@
-/* *
- * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
- * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
- * session persistence, api calls, and more.
- * */
-const Alexa = require('ask-sdk-core');
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Licensed under the Amazon Software License
+// http://aws.amazon.com/asl/
 
-const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+/* eslint-disable  no-restricted-syntax */
+const Alexa = require('ask-sdk');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+const languageStrings = {
+  'en': require('./languageStrings')
+}
+const AWS = require('aws-sdk');
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
+const LaunchRequest = {
+  canHandle(handlerInput) {
+    // launch requests as well as any new session, as games are not saved in progress, which makes
+    // no one shots a reasonable idea except for help, and the welcome message provides some help.
+    return Alexa.isNewSession(handlerInput.requestEnvelope) 
+      || Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  async handle(handlerInput) {
+    const { attributesManager } = handlerInput;
+    const requestAttributes = attributesManager.getRequestAttributes();
+    const attributes = await attributesManager.getPersistentAttributes() || {};
+
+    if (Object.keys(attributes).length === 0) {
+      attributes.endedSessionCount = 0;
+      attributes.gamesPlayed = 0;
+      attributes.gameState = 'ENDED';
     }
+
+    attributesManager.setSessionAttributes(attributes);
+
+    const gamesPlayed = attributes.gamesPlayed.toString()
+    const speechOutput = requestAttributes.t('LAUNCH_MESSAGE', gamesPlayed);
+    const reprompt = requestAttributes.t('CONTINUE_MESSAGE');
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(reprompt)
+      .getResponse();
+  },
 };
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
+const ExitHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+        || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('EXIT_MESSAGE'))
+      .getResponse();
+  },
 };
 
-const HelpIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
+const SessionEndedRequest = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
 };
 
-const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Goodbye!';
+const HelpIntent = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('HELP_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .getResponse();
+  },
 };
-/* *
- * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
- * It must also be defined in the language model (if the locale supports it)
- * This handler can be safely added but will be ingnored in locales that do not support it yet 
- * */
-const FallbackIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
-/* *
- * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
- * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
- * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
- * */
-const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
-    }
-};
-/* *
- * The intent reflector is used for interaction model testing and debugging.
- * It will simply repeat the intent the user said. You can create custom handlers for your intents 
- * by defining them above, then also adding them to the request handler chain below 
- * */
-const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `You just triggered ${intentName}`;
+const YesIntent = {
+  canHandle(handlerInput) {
+    // only start a new game if yes is said when not playing a game.
+    let isCurrentlyPlaying = false;
+    const { attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
+    if (sessionAttributes.gameState &&
+      sessionAttributes.gameState === 'STARTED') {
+      isCurrentlyPlaying = true;
     }
+
+    return !isCurrentlyPlaying 
+      && Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.YesIntent';
+  },
+  handle(handlerInput) {
+    const { attributesManager } = handlerInput;
+    const requestAttributes = attributesManager.getRequestAttributes();
+    const sessionAttributes = attributesManager.getSessionAttributes();
+
+    sessionAttributes.gameState = 'STARTED';
+    sessionAttributes.guessNumber = Math.floor(Math.random() * 101);
+
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('YES_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .getResponse();
+  },
 };
-/**
- * Generic error handling to capture any syntax or routing errors. If you receive an error
- * stating the request handler chain is not found, you have not implemented a handler for
- * the intent being invoked or included it in the skill builder below 
- * */
+
+const NoIntent = {
+  canHandle(handlerInput) {
+    // only treat no as an exit when outside a game
+    let isCurrentlyPlaying = false;
+    const { attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
+
+    if (sessionAttributes.gameState &&
+      sessionAttributes.gameState === 'STARTED') {
+      isCurrentlyPlaying = true;
+    }
+
+    return !isCurrentlyPlaying 
+      && Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NoIntent';
+  },
+  async handle(handlerInput) {
+    const { attributesManager } = handlerInput;
+    const requestAttributes = attributesManager.getRequestAttributes();
+    const sessionAttributes = attributesManager.getSessionAttributes();
+
+    sessionAttributes.endedSessionCount += 1;
+    sessionAttributes.gameState = 'ENDED';
+    attributesManager.setPersistentAttributes(sessionAttributes);
+
+    await attributesManager.savePersistentAttributes();
+
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('EXIT_MESSAGE'))
+      .getResponse();
+
+  },
+};
+
+const UnhandledIntent = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('CONTINUE_MESSAGE'))
+      .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const NumberGuessIntent = {
+  canHandle(handlerInput) {
+    // handle numbers only during a game
+    let isCurrentlyPlaying = false;
+    const { attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
+
+    if (sessionAttributes.gameState &&
+      sessionAttributes.gameState === 'STARTED') {
+      isCurrentlyPlaying = true;
+    }
+
+    return isCurrentlyPlaying 
+      && Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'NumberGuessIntent';
+  },
+  async handle(handlerInput) {
+    const { attributesManager } = handlerInput;
+    const requestAttributes = attributesManager.getRequestAttributes();
+    const sessionAttributes = attributesManager.getSessionAttributes();
+
+    const guessNum = parseInt(Alexa.getSlotValue(handlerInput.requestEnvelope, 'number'), 10);
+    const targetNum = sessionAttributes.guessNumber;
+    
+    if (guessNum > targetNum) {
+      return handlerInput.responseBuilder
+        .speak(requestAttributes.t('TOO_HIGH_MESSAGE', guessNum.toString()))
+        .reprompt(requestAttributes.t('TOO_HIGH_REPROMPT'))
+        .getResponse();
+    } else if (guessNum < targetNum) {
+      return handlerInput.responseBuilder
+        .speak(requestAttributes.t('TOO_LOW_MESSAGE', guessNum.toString()))
+        .reprompt(requestAttributes.t('TOO_LOW_REPROMPT'))
+        .getResponse();
+    } else if (guessNum === targetNum) {
+      sessionAttributes.gamesPlayed += 1;
+      sessionAttributes.gameState = 'ENDED';
+      attributesManager.setPersistentAttributes(sessionAttributes);
+      await attributesManager.savePersistentAttributes();
+      return handlerInput.responseBuilder
+        .speak(requestAttributes.t('GUESS_CORRECT_MESSAGE', guessNum.toString()))
+        .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
+        .getResponse();
+    }
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('FALLBACK_MESSAGE_DURING_GAME'))
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT_DURING_GAME'))
+      .getResponse();
+  },
+};
+
 const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
-        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('ERROR_MESSAGE'))
+      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
+      .getResponse();
+  },
 };
 
-/**
- * This handler acts as the entry point for your skill, routing all request and response
- * payloads to the handlers above. Make sure any new handlers or interceptors you've
- * defined are included below. The order matters - they're processed top to bottom 
- * */
-exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        FallbackIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler)
-    .addErrorHandlers(
-        ErrorHandler)
-    .withCustomUserAgent('sample/hello-world/v1.2')
-    .lambda();
+const FallbackHandler = {
+  canHandle(handlerInput) {
+    // handle fallback intent, yes and no when playing a game
+    // for yes and no, will only get here if and not caught by the normal intent handler
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent' 
+      || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.YesIntent'
+      || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NoIntent');
+  },
+  handle(handlerInput) {
+    const { attributesManager } = handlerInput;
+    const requestAttributes = attributesManager.getRequestAttributes();
+    const sessionAttributes = attributesManager.getSessionAttributes();
+
+    if (sessionAttributes.gameState && sessionAttributes.gameState === 'STARTED') {
+      // currently playing
+      return handlerInput.responseBuilder
+        .speak(requestAttributes.t('FALLBACK_MESSAGE_DURING_GAME'))
+        .reprompt(requestAttributes.t('FALLBACK_REPROMPT_DURING_GAME'))
+        .getResponse();
+    }
+
+    // not playing
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('FALLBACK_MESSAGE_OUTSIDE_GAME'))
+      .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    const localizationClient = i18n.use(sprintf).init({
+      lng: Alexa.getLocale(handlerInput.requestEnvelope),
+      resources: languageStrings,
+    });
+    localizationClient.localize = function localize() {
+      const args = arguments;
+      const values = [];
+      for (let i = 1; i < args.length; i += 1) {
+        values.push(args[i]);
+      }
+      const value = i18n.t(args[0], {
+        returnObjects: true,
+        postProcess: 'sprintf',
+        sprintf: values,
+      });
+      if (Array.isArray(value)) {
+        return value[Math.floor(Math.random() * value.length)];
+      }
+      return value;
+    };
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function translate(...args) {
+      return localizationClient.localize(...args);
+    };
+  },
+};
+
+function getPersistenceAdapter() {
+  // Determines persistence adapter to be used based on environment
+  const dynamoDBAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+  return new dynamoDBAdapter.DynamoDbPersistenceAdapter({
+    tableName: process.env.DYNAMODB_PERSISTENCE_TABLE_NAME,
+    createTable: false,
+    dynamoDBClient: new AWS.DynamoDB({apiVersion: 'latest', region: process.env.DYNAMODB_PERSISTENCE_REGION})
+  });
+}
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .withPersistenceAdapter(getPersistenceAdapter())
+  .addRequestHandlers(
+    LaunchRequest,
+    ExitHandler,
+    SessionEndedRequest,
+    HelpIntent,
+    YesIntent,
+    NoIntent,
+    NumberGuessIntent,
+    FallbackHandler,
+    UnhandledIntent,
+  )
+  .addRequestInterceptors(LocalizationInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
